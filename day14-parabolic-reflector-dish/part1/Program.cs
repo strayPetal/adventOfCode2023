1f866c216e37868474fc234f77faa5cca7c084f3
Stray Petal <150872093+strayPetal@users.noreply.github.com>
2023-12-15 10:37:03 +1300

    [2023 14-1] it's done! it's messy, but it's done!

---
+++ day14-parabolic-reflector-dish/part1/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+    Beware! The messiest code ever is incoming!
+*/
+
+using System;
+using System.IO;
+using System.Linq;
+
+class Program {
+    const string PUZZLE_INPUT_FILE = "input.txt";
+    
+    const char EMPTY_SPACE = '.';
+    const char ROUNDED_ROCK = 'O';
+    const char CUBE_ROCK = '#';
+    
+    static void Main(string[] args) {
+        var mirrorRows = File.ReadLines(PUZZLE_INPUT_FILE);
+        
+        int[] mirrorFreeSpaces = new int[mirrorRows.First().Length];
+        Array.Fill(mirrorFreeSpaces, 0);
+
+        int totalLoad = 0;
+
+        for (int rowIndex = 0; rowIndex < mirrorRows.Count(); rowIndex++) {
+            string row = mirrorRows.ElementAt(rowIndex);
+            for (int spaceInRow = 0; spaceInRow < row.Length; spaceInRow++) {
+                char spot = row[spaceInRow];
+                if (spot == ROUNDED_ROCK) {
+                    int moveTo = mirrorFreeSpaces[spaceInRow] + 1;
+                    totalLoad += mirrorRows.Count() - mirrorFreeSpaces[spaceInRow];
+                    mirrorFreeSpaces[spaceInRow]++;
+                } else if (spot == CUBE_ROCK) {
+                    mirrorFreeSpaces[spaceInRow] = rowIndex + 1;
+                }
+            }
+        }
+
+        Console.WriteLine($"Total load on support beams along north side of platform is: {totalLoad}");
+    }
+}
